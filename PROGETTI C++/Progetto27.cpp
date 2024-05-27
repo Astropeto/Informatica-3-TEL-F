35ff04a074d513f78686a94f8eccf8c5417779b3
Astro peto <simone.clemente@studenti.malignani.ud.it>
2024-05-27 08:36:56 +0000

    prova in classe

---
+++ PROGETTI C++/Progetto27.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    cout<<"Benvenuto su GITPOD"<<endl;
+
+    string nome;
+    cout<<"Scrivi il tuo nome: ";
+    cin>>nome;
+
+    cout<<"Ciao "<<nome<<endl;
+
+    return 0;
+}
